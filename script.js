d1e9168fe263e4ee0072cd372053f637b164a4d2
Luiz Guilherme Carvalho Ribeiro <guigo17ribeiro@gmail.com>
2026-05-08 14:38:14 -0300

    separação de conteúdos em html, css e js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+             function Apresentacao() {
+                document.getElementById('inicio').innerHTML='<h1>olá meu nome é luiz guilherme</h1>';
+             }
+
+             function alerta() {
+                alert("você sera redirecionado a outra página")
+             }
